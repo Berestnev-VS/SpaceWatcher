8d93f07322dea4facb31761aa2f10789cd6ffbc8
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-18 19:20:42 +0300

    iteraction 1

--- SpaceXContur/Views/MainCollectionViewCell.swift
+++ SpaceXContur/Views/MainCollectionViewCell.swift
@@ -11,47 +11,54 @@ class MainCollectionViewCell: UICollectionViewCell, UITableViewDataSource, UITab
     
     static let identifier = "MainCollectionViewCell"
     
-    weak var delegate: MainCollectionViewCellDelegate?
-    
-    var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        return tableView
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.register(DataTableViewCell.self, forCellReuseIdentifier: DataTableViewCell.identifier)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(tableView)
-        tableView.isScrollEnabled = false
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-        ])
-        
-        tableView.delegate = self
+        contentView.addSubview(tableView)
+        contentView.backgroundColor = .systemPink
+        tableView.backgroundColor = .systemRed
         tableView.dataSource = self
+        tableView.delegate = self
+
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Cell \(indexPath.row)"
-        return cell
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        delegate?.updateHeightOfRow(self, tableView)
+        contentView.layer.cornerRadius = 12
+        contentView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DataTableViewCell.identifier, for: indexPath) as! DataTableViewCell
+        cell.configure(with: ["Description", "Value"])
+        return cell
+    }
 }
